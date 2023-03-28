fe69cc86d83790b00a01b4e83bb5c1a92997c956
irvan12124 <75716739+irvan12124@users.noreply.github.com>
2023-03-28 21:38:04 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/@core/layouts/components/AppFooter.vue
@@ -0,0 +1,28 @@
+<template>
+  <p class="clearfix mb-0">
+    <span class="float-md-left d-none d-md-block d-md-inline-block mt-25">
+      <b-link
+        class="ml-25 font-weight-bolder"
+        href="#"
+        target="_blank"
+      >NodeStake</b-link>
+    </span>
+
+  </p>
+</template>
+
+<script>
+import { BLink, VBPopover } from 'bootstrap-vue'
+import Ripple from 'vue-ripple-directive'
+
+export default {
+  name: 'AppFooter',
+  components: {
+    BLink,
+  },
+  directives: {
+    'b-popover': VBPopover,
+    Ripple,
+  },
+}
+</script>
---
+++ src/navigation/vertical/index.js
@@ -0,0 +1,58 @@
+import store from '@/store'
+import { isTestnet } from '../../libs/utils'
+
+function processMenu() {
+  const chainMenus = []
+  const blockchains = []
+  Object.keys(store.state.chains.config).forEach(chain => {
+    const menu = {
+      title: chain,
+      logo: store.state.chains.config[chain].logo,
+      route: { name: 'dashboard', params: { chain } },
+    }
+    blockchains.push(menu)
+  })
+
+  if (blockchains.length > 1) {
+    chainMenus.push({ header: 'ecosystem' })
+    chainMenus.push({
+      title: 'chains',
+      children: blockchains,
+      tag: `${blockchains.length}`,
+      icon: 'ChromeIcon',
+    })
+  }
+  chainMenus.push({ header: 'LINKS' })
+  if (isTestnet()) {
+    chainMenus.push({
+      title: 'Mainnet Explorer',
+      href: 'https://explorer.nodestake.top',
+      icon: 'ChromeIcon',
+    })
+  } else {
+    chainMenus.push({
+      title: 'Website',
+      href: 'http://nodestake.top',
+      icon: 'LifeBuoyIcon',
+    })
+  }
+  chainMenus.push({
+    title: 'Discord',
+    href: 'https://discord.gg/puGTkXvkE8',
+    icon: 'MessageSquareIcon',
+  })
+  chainMenus.push({
+    title: 'Twitter',
+    href: 'https://twitter.com/Nodestake_top',
+    icon: 'TwitterIcon',
+  })
+  chainMenus.push({
+    title: 'REStake',
+    href: 'https://restake.nodestake.top',
+    icon: 'CastIcon',
+  })
+
+  return chainMenus
+}
+
+export default processMenu()
